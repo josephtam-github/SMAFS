0578e235165ce8937d134fb8e6152304817af1ab
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-13 17:42:54 +0100

    feat: instantiate and configure Api to flask app

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_smorest import Api
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -7,7 +8,15 @@ db_name = 'smafs.db'
 
 app = Flask(__name__)
 app.app_context().push()
+
+# SQLAlchemy configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, db_name)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+# Api configuration
+app.config["API_TITLE"] = "My API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.2"
+api = Api(app)
