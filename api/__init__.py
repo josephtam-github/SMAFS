ad856862c1c2c89a29e4cae70f0a18881eb5e289
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-13 16:39:33 +0100

    refactor: set db location with os.path.realpath()

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+db_name = 'smafs.db'
 
 app = Flask(__name__)
 app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///smafs.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, db_name)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
