ed128a98c170dc88e5a93a50f74fbe94d5fc2f1b
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-14 23:39:37 +0100

    feat: create admin requirement decorator

---
+++ api/utils/decorator.py
@@ -0,0 +1,18 @@
+from flask_jwt_extended import get_jwt, verify_jwt_in_request
+from functools import wraps
+from http import HTTPStatus
+
+
+def admin_required():
+    """verifies if token holder is an admin"""
+    def wrapper(f):
+        @wraps(f)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if claims["is_admin"]:
+                return f(*args, **kwargs)
+            else:
+                return {"message": "Administrator access required"}, HTTPStatus.FORBIDDEN
+        return decorator
+    return wrapper
