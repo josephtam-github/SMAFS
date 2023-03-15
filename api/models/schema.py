5384c788caabf90154cf29ff59f5c7fa261f6a41
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-15 21:32:07 +0100

    refactor: rename student model to user model
    build: change is_admin column to category

--- api/models/schema.py
+++ api/models/schema.py
@@ -2,24 +2,24 @@ import marshmallow as mar
 from marshmallow import EXCLUDE
 from marshmallow.validate import Length
 from marshmallow_sqlalchemy import field_for
-from ..models.student import Student
+from ..models.user import User
 from api import ma
 from ..models.course import Course
 from ..models.record import Record
 
 
-class StudentSchema(ma.SQLAlchemySchema):
+class UserSchema(ma.SQLAlchemySchema):
     class Meta:
-        model = Student
+        model = User
         ordered = True
         unknown = EXCLUDE
 
-    student_id = field_for(Student, "student_id", dump_only=True)
-    firstname = field_for(Student, "firstname", required=True, validate=Length(min=2, max=45))
-    lastname = field_for(Student, "lastname", required=True, validate=Length(min=2, max=45))
-    email = field_for(Student, "email", required=True, validate=Length(min=5, max=50))
-    is_admin = field_for(Student, "is_admin", dump_only=True)
-    password = field_for(Student, "password_hash", required=True)
+    user_id = field_for(User, "user_id", dump_only=True)
+    firstname = field_for(User, "firstname", required=True, validate=Length(min=2, max=45))
+    lastname = field_for(User, "lastname", required=True, validate=Length(min=2, max=45))
+    email = field_for(User, "email", required=True, validate=Length(min=5, max=50))
+    category = field_for(User, "category", dump_only=True)
+    password = field_for(User, "password_hash", required=True)
 
     def update(self, obj, data):
         """Update object nullifying missing data"""
@@ -30,15 +30,15 @@ class StudentSchema(ma.SQLAlchemySchema):
             setattr(obj, name, data.get(name))
 
 
-class StudentQueryArgsSchema(mar.Schema):
+class UserQueryArgsSchema(mar.Schema):
     class Meta:
         unknown = EXCLUDE
 
-    student_id = mar.fields.Integer()
+    user_id = mar.fields.Integer()
     firstname = mar.fields.String(validate=Length(min=2, max=45))
     lastname = mar.fields.String(validate=Length(min=2, max=45))
     email = mar.fields.String(validate=Length(min=2, max=50))
-    is_admin = mar.fields.Boolean()
+    category = mar.fields.String(validate=Length(min=2, max=50))
 
 
 class LoginQueryArgsSchema(mar.Schema):
--- api/models/user.py
+++ api/models/user.py
@@ -1,17 +1,17 @@
 from api import db
 
 
-class Student(db.Model):
-    __tablename__ = "student"
-    student_id = db.Column(db.Integer(), primary_key=True)
+class User(db.Model):
+    __tablename__ = "user"
+    user_id = db.Column(db.Integer(), primary_key=True)
     firstname = db.Column(db.String(45), nullable=False)
     lastname = db.Column(db.String(45), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     password_hash = db.Column(db.Text(), nullable=False)
-    is_admin = db.Column(db.Boolean(), default=False)
+    category = db.Column(db.String(50), default="STUDENT")
 
     def __repr__(self):
-        return f"<Student {self.student_id}>"
+        return f"<User {self.user_id}>"
 
     def save(self):
         db.session.add(self)
@@ -21,8 +21,8 @@ class Student(db.Model):
         db.session.commit()
 
     @classmethod
-    def get_by_id(cls, student_id):
-        return cls.query.get_or_404(student_id)
+    def get_by_id(cls, user_id):
+        return cls.query.get_or_404(user_id)
 
     def delete(self):
         db.session.delete(self)
