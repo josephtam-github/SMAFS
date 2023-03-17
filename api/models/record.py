55b94bef523c74a5bfac5e93d53795b3b47efa08
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-17 21:08:48 +0100

    refactor: remove auth on record model save() method

--- api/models/record.py
+++ api/models/record.py
@@ -1,11 +1,12 @@
 from api import db
 from .user import User
 
+
 class Record(db.Model):
     __tablename__ = "record"
 
-    student_id = db.Column(db.Integer(), db.ForeignKey("user.user_id"), unique=True, nullable=False)
-    course_id = db.Column(db.Integer(), db.ForeignKey("course.course_id"), unique=True, nullable=False)
+    student_id = db.Column(db.Integer(), db.ForeignKey("user.user_id"), nullable=False)
+    course_id = db.Column(db.Integer(), db.ForeignKey("course.course_id"), nullable=False)
     score = db.Column(db.Integer(), nullable=True)
 
     __table_args__ = (
@@ -18,11 +19,13 @@ class Record(db.Model):
         return f"<Score {self.score}>"
 
     def save(self):
-        user = User.query.filter(user_id=self.student_id, category='STUDENT').\
-            first_or_404(description='There is no student with id: {}'.format(self.student_id))
-        if user is not None:
-            db.session.add(self)
-            db.session.commit()
+        # user = User.query.filter(user_id=self.student_id, category='STUDENT').\
+        #     first_or_404(description='There is no student with id: {}'.format(self.student_id))
+        # course = User.query.filter(course_id=self.course_id).\
+        #     first_or_404(description='There is no course with id: {}'.format(self.student_id))
+        # if user is not None:
+        db.session.add(self)
+        db.session.commit()
 
     def update(self):
         db.session.commit()
