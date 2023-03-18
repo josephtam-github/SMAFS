729848b93ce64e8120153bb3518f0614618301d9
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-18 04:59:09 +0100

    feat: create app factory and import config
    fix: remove wrong decouple dependency

--- api/__init__.py
+++ api/__init__.py
@@ -1,44 +1,61 @@
+from dotenv import load_dotenv
 from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 from flask_smorest import Api, Blueprint
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_jwt_extended import JWTManager
-from datetime import timedelta
-import os
+from .config.config import config_dict
+from .utils import db, ma
+from .models.blocklist import TokenBlocklist
+from .models.user import User
+from .models.course import Course
+from .models.record import Record
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-db_name = 'smafs.db'
 
-app = Flask(__name__)
-app.app_context().push()
+def create_app(config=config_dict['dev']):
+    app = Flask(__name__)
 
-# SQLAlchemy configuration & instantiation
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, db_name)
-app.config['SECRET_KEY'] = os.urandom(24)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+    app.config.from_object(config)
 
-# Marshmallow instantiation
-ma = Marshmallow(app)
 
-# Api configuration & instantiation
-app.config["API_TITLE"] = "My API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.2"
-api = Api(app)
+    db.init_app(app)
+    ma.init_app(app)
+    migrate = Migrate(app, db)
 
-# JWT configuration & instantiation
-app.config["JWT_SECRET_KEY"] = os.urandom(24)
-ACCESS_EXPIRES = timedelta(hours=1)
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
-jwt = JWTManager(app)
+    app.config["API_TITLE"] = "My API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.2"
+    api = Api(app)
 
-# Import blueprints at end of file to prevent circular import
-from .auth.views import auth
-from .students.views import student
-from .courses.views import course
-from .record.views import record
-api.register_blueprint(auth)
-api.register_blueprint(student)
-api.register_blueprint(course)
-api.register_blueprint(record)
+    jwt = JWTManager(app)
+
+    # Callback function to check if a JWT exists in the database blocklist
+    @jwt.token_in_blocklist_loader
+    def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
+        jti = jwt_payload["jti"]
+        token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
+
+        return token is not None
+
+    # Import blueprints at end of file to prevent circular import
+    from .auth.views import auth
+    from .students.views import student
+    from .courses.views import course
+    from .record.views import record
+    api.register_blueprint(auth)
+    api.register_blueprint(student)
+    api.register_blueprint(course)
+    api.register_blueprint(record)
+
+    @app.shell_context_processor
+    def make_shell_context():
+        return {
+            'db': db,
+            'ma': ma,
+            'User': User,
+            'Course': Course,
+            'Record': Record,
+        }
+
+    return app
