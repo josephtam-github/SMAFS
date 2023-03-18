31009e784434663a5c26750b7d36972f6ec92285
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-18 01:59:35 +0100

    feat: create grade to gpa converter

---
+++ api/utils/gpa.py
@@ -0,0 +1,11 @@
+def grade_to_gpa(letter_grade):
+    if letter_grade == 'A':
+        return 4.0
+    elif letter_grade == 'B':
+        return 3.3
+    elif letter_grade == 'C':
+        return 2.3
+    elif letter_grade == 'D':
+        return 1.3
+    else:
+        return 0
