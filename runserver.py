744def69266c328da0f83654ba7828af25db5385
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-18 05:03:07 +0100

    feat: initialize flask app from factory

--- runserver.py
+++ runserver.py
@@ -1,5 +1,9 @@
-from api import app
+from api import create_app, db
+from api.config.config import config_dict
 
+app = create_app(config=config_dict['test'])
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
