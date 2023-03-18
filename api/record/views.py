750bb794391f050d3456e57639fb37973d38fa7e
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-03-18 01:41:43 +0100

    feat: create resource to show and add score to a students course

--- api/record/views.py
+++ api/record/views.py
@@ -1,13 +1,16 @@
 from flask_smorest import Blueprint, abort
 from flask.views import MethodView
+from sqlalchemy import Null
 from ..models.user import User
 from ..models.record import Record
 from ..models.course import Course
-from ..models.schema import CourseSchema, RecordSchema, StudentSchema
+from ..models.schema import RecordSchema, StudentSchema, ScoreSchema, UpdateScoreArgsSchema, ScoreArgsSchema
 from http import HTTPStatus
 from flask import jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from api import db
 from ..utils.decorator import admin_required
+from ..utils.grader import letter_grade
 
 record = Blueprint(
     'Record',
@@ -17,6 +20,7 @@ record = Blueprint(
 )
 
 
+# Admin resource - only admin should see all who registered for a course
 @record.route('/course/')
 @record.route('/course/<int:course_id>')
 class GetStudentsOnCourse(MethodView):
@@ -39,6 +43,83 @@ class GetStudentsOnCourse(MethodView):
                     result.append(result_dict)
                 return jsonify(result), HTTPStatus.OK
             else:
-                abort(HTTPStatus.NO_CONTENT, message='There are currently no registered students')
+                abort(HTTPStatus.NOT_FOUND, message='There are currently no registered students')
         else:
             abort(HTTPStatus.NOT_FOUND, message='Course does not exist')
+
+
+# Student score resource - Show current score in a course
+@record.route('/student/<int:course_id>')
+class GetStudentCourseScore(MethodView):
+    @record.response(HTTPStatus.OK, ScoreSchema, description='Returns an object containing'
+                                                             ' students score in a course')
+    @jwt_required()
+    def get(self, course_id):
+        """Get students score for a specified course"""
+        course_exist = Course.query.filter_by(course_id=course_id).first()
+        student_id = get_jwt_identity()
+
+        if course_exist:
+            score = Record.query.filter_by(course_id=course_id, student_id=student_id).first()
+            if score is not None:
+                result = []
+                if score.score is Null:
+                    score_val = "You haven't been scored yet"
+                    grade_val = "You haven't been graded yet"
+                else:
+                    score_val = score.score
+                    grade_val = letter_grade(score.score)
+
+                result_dict = {'name': course_exist.name,
+                               'teacher': course_exist.teacher,
+                               'credit': course_exist.credit,
+                               'score': score_val,
+                               'grade': grade_val
+                               }
+
+                result.append(result_dict)
+                return jsonify(result), HTTPStatus.OK
+            else:
+                abort(HTTPStatus.NOT_FOUND, message='You have not been registered for this course')
+        else:
+            abort(HTTPStatus.NOT_FOUND, message='Course does not exist')
+
+
+@record.route('/<int:course_id>/')
+@record.route('/<int:course_id>/<int:student_id>')
+class StudentCourseScoreById(MethodView):
+    @record.arguments(UpdateScoreArgsSchema)
+    @record.response(HTTPStatus.CREATED, ScoreArgsSchema, description='Returns an object containing'
+                                                                      ' students course data with new score')
+    @admin_required()
+    def put(self, score_data, course_id, student_id):
+        """Add score to specified student, who has been registered for the specified course"""
+
+        student_data = User.query.filter_by(user_id=student_id, category='STUDENT').first()
+
+        # check if user requested student exist
+        if student_data is not None:
+            course_data = Course.query.filter_by(course_id=course_id).first()
+            if course_data is not None:
+                record_exist = Record.query.filter_by(course_id=course_id, student_id=student_id).first()
+                if record_exist:
+                    record_exist.score = int(score_data['score'])
+                    record_exist.save()
+
+                    result = []
+                    result_dict = {'name': course_data.name,
+                                   'firstname': student_data.firstname,
+                                   'lastname': student_data.lastname,
+                                   'matric_no': student_data.matric_no,
+                                   'score': record_exist.score,
+                                   'grade': letter_grade(record_exist.score)
+                                   }
+
+                    result.append(result_dict)
+                    return jsonify(result), HTTPStatus.CREATED
+                else:
+                    abort(HTTPStatus.NOT_FOUND, message='Student has not been registered')
+            else:
+                abort(HTTPStatus.NOT_FOUND, message='Course does not exist')
+        else:
+            abort(HTTPStatus.NOT_FOUND, message='Student does not exist')
